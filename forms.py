84bede86449cdbad847e35b646b04b58eb1c2111
baihtjs <704661485@qq.com>
2020-06-09 17:55:49 +0800

    2020-6-5-comp

--- forms.py
+++ forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired,Length
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, DateTimeField, FileField
+from wtforms.validators import DataRequired, Length, Email
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message='too')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message='password-too-short')])
     #password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=5, message="长度不符合")])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log in')
+    date = DateField('Date')
+    datetime = DateTimeField('DateTime')
+    email = StringField('Email', validators=[DataRequired(), Email(message='error email format ')])
+
+class UploadForm(FlaskForm):
+    photo = FileField('Upload Image', validators=[FileRequired(),FileAllowed['jpg', 'png'], 'Images only!'])
+    submit = SubmitField()
+
